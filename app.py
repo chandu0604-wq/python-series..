d320e3fe9ab3867d4a75e102683d616c79cf9350
Hruthik D Gowda <chandud961@gmail.com>
2026-03-14 23:36:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,94 @@
+
+
+
+# input1="hello"
+
+# revrser=input1[::-1]
+# print(revrser) 
+
+# i1="madam"
+
+# if i1==i1[::-1]:
+#     print("true")
+
+# ________________________________
+
+
+# n=5 
+
+# a=0
+# b=1 
+
+# for i in range(n):
+#     print(a,end=" ")
+#     c=a+b
+#     a=b
+#     b=c
+# _______________________
+
+
+# n1=[1,2,2,3,4,4]
+
+# result=[]
+
+
+# for i in n1:
+#     if i not in result:
+#         result.append(i)
+# print(result)
+
+# __________________________
+
+# n=[23,45,50,23,99]
+
+
+# result=sorted(n)
+
+# final=result[-2]
+# print(final)
+
+# _____________________
+
+# w1="listen"
+# w2="silent"
+# count=0
+# for i in w1:
+#     if i in w2:
+#         count+=1 
+# if count==len(w2):
+#     print("Anagram")
+# else:
+#     print("Not")
+
+# _______________________
+
+# if sorted(w1)==sorted(w2):
+#     print("Anagram") 
+
+# _ ____ ____ _____ _____ _
+
+# l1=[10,20,30,40,50]
+# target=30 
+
+# for i in range(len(l1)):
+#     if l1[i]==target:
+#         print("Found at index: " ,i) 
+
+# ___________________________
+
+# s="({[]})" 
+
+# while "()" in s or "{}" in s or "[]" in s:
+#     s=s.replace("()","").replace("{}","").replace("[]","")
+
+# if (s==""):
+#     print("Balanced")
+# else:
+#     print("Not balanced")
+
+
+
+
+
+
+
